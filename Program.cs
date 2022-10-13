bf7b14cb893fc029d819759c9a6550beb5a76210
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-13 12:01:37 +0530

    Display the Message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation!");
+            SwitchCase.CalConditionalWage();
+            EmployeeCheck.EmployeeAttendance();
+            EmpWageBuilerArray empWageBuilerArray = new EmpWageBuilerArray();
+
+            empWageBuilerArray.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilerArray.addCompanyEmpWage("Reilance", 10, 4, 20);
+
+            empWageBuilerArray.computeEmpWage();
+            Console.WriteLine("Total Wage for DMart company: " + empWageBuilerArray.getTotalEmpWage(0));
+        }
+    }
+}
