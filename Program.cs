7a201e0c08c758addf004081cfd0af89e1ca8633
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-13 12:08:28 +0530

    Usecase 1

--- Program.cs
+++ Program.cs
@@ -5,15 +5,15 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
-            SwitchCase.CalConditionalWage();
+            //SwitchCase.CalConditionalWage();
             EmployeeCheck.EmployeeAttendance();
-            EmpWageBuilerArray empWageBuilerArray = new EmpWageBuilerArray();
+            //EmpWageBuilerArray empWageBuilerArray = new EmpWageBuilerArray();
 
-            empWageBuilerArray.addCompanyEmpWage("DMart", 20, 2, 10);
-            empWageBuilerArray.addCompanyEmpWage("Reilance", 10, 4, 20);
+            //empWageBuilerArray.addCompanyEmpWage("DMart", 20, 2, 10);
+            //empWageBuilerArray.addCompanyEmpWage("Reilance", 10, 4, 20);
 
-            empWageBuilerArray.computeEmpWage();
-            Console.WriteLine("Total Wage for DMart company: " + empWageBuilerArray.getTotalEmpWage(0));
+            //empWageBuilerArray.computeEmpWage();
+            //Console.WriteLine("Total Wage for DMart company: " + empWageBuilerArray.getTotalEmpWage(0));
         }
     }
 }
